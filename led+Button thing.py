a35c0c25ec773775c5b6f5079e02b26357cfe62c
Dhruv-Mistry <52937716+Dhruv-Mistry@users.noreply.github.com>
2019-07-19 11:43:06 -0700

    Add files via upload
    shuffled.

--- led+Button thing.py
+++ led+Button thing.py
@@ -4,39 +4,46 @@ from gpiozero import LED
 
 from time import sleep
 
-red = LED(4)
+from random import shuffle 
 
-green = LED(17)
+red = LED(19)
 
-LeftButton = Button(22)
+green = LED(4)
 
-RightButton = Button(27)
+LeftButton = Button(17)
+RightButton = Button(26)
 
-red.on()
-sleep (.2)
-red.off()
-sleep (.2)
-green.on()
-sleep (.2)
-green.off()
-sleep (.2)
-green.on()
-sleep (.2)
-green.off()
-red.on()
-sleep (.2)
-red.off()
 
-while True:
+lst = [red, green, red, red, green, green, red]
+shuffle(lst)
+
+for led in lst:
+    led.on()
+    sleep(.2)
+    led.off()
+    sleep(.1)
+
+userSelection = []
+
+while len(userSelection) < len(lst):
     if LeftButton.is_pressed:
-    if RightButton.is_pressed:
-    if RightButton.is_pressed:
-    if LeftButton.is_pressed:
-        green.on()
-    else:
-        red.on()
+        print("Green Pressed!")
+        userSelection.append(green)
+        LeftButton.wait_for_release()    
+    elif RightButton.is_pressed:
+        print("Red pressed!")
+        userSelection.append(red)
+        RightButton.wait_for_release()
+    
+hasWon = True        
+for i in range(len(lst)):
+    if lst[i] != userSelection[i]:
+        hasWon = False
+        break
+if not hasWon:
+    print("You lose!")
+else:
+    print("You win!")
+    
+    
 
-    
-    
-    
-    
